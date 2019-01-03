c10a1cfcf9a7e2aa37e781dd932307e0456b3fba
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-03 15:31:07 -0800

    All components load data based on stock id in url

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 6543;
 
-app.use(express.static('public'));
+app.use('/', express.static('./public'));
+
+// Route any path with pattern /number/ to the public directory as well
+app.use(/\/\d+\//, express.static('./public'));
 
 app.listen(port, () => {console.log('Proxy server is listening on port ' + port)})
